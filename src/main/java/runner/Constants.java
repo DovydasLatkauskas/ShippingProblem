8b5efa17632926892a27a797adb9fea12ed50f4e
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-04-15 22:50:57 +0100

    created createDiscountsLeftEachMonth

--- src/main/java/runner/Constants.java
+++ src/main/java/runner/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     // path to folder of input transaction file input.txt
     public static final String INPUT_FOLDER_PATH = "input/";
     // maximum allowed discount per month in cents
-    public static final int TOTAL_DISCOUNT_MONTH_CENTS = 1000;
+    public static final Integer TOTAL_DISCOUNT_MONTH_CENTS = 1000;
 }
